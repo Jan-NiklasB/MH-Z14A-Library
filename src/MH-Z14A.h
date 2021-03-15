d123e57e34dcc5716329d36744df8a4d5f8f627a
Jan-NiklasB <44967908+Jan-NiklasB@users.noreply.github.com>
2021-03-15 18:18:47 +0100

    Added and expanded methods

--- src/MH-Z14A.h
+++ src/MH-Z14A.h
@@ -14,18 +14,27 @@ class MHZ14A{
     public:
         MHZ14A(HardwareSerial &serial);                             //Constructor only with UART
         MHZ14A(HardwareSerial &serial, HardwareSerial &logSerial);  //Constructor with UART and Hardwareserial port for debuging
-        MHZ14A(HardwareSerial &serial, Serial_ &logSerial);         //Constructor with UART and nativ serial port for debuging (for example SerialUSB on Arduino Due)
+        #if defined(Serial_)
+            MHZ14A(HardwareSerial &serial, Serial_ &logSerial);         //Constructor with UART and nativ serial port for debuging (for example SerialUSB on Arduino Due)
+        #endif
         void begin(int timeout);                                    //Method for initializing the UART and debug port
         int readConcentrationPPM(byte sensor);                      //Function for reading the CO2-concentration in PPM
         void calZeroPoint(byte sensor);                             //Method to set the zero point (refere to manual)
-        void calSpanPoint(byte sensor);                             //Method to set the zero span (refere to manual)
+        void calSpanPoint(byte sensor, uint8_t span_value_ppm);     //Method to set the zero span (refere to manual), span_value_ppm should be above 1000
+        void setDetectionRange(byte sensor, byte Range[4]);
+        void setAutoCal(byte sensor, bool state);
         void setDebug(bool state);                                  //Method to set the debug mode if debug port is specified
+        const byte MR_2000[4] = {0x00, 0x00, 0x07, 0xD0};
+        const byte MR_5000[4] = {0x00, 0x00, 0x13, 0x88};
+        const byte MR_10000[4] = {0x00, 0x00, 0x27, 0x10}; 
     private:
         byte crc(byte msg[9]);                                      //Function to calculate the checksum
         void log(byte msg[9]);                                      //Method to send the debug messages to the serial interface
         HardwareSerial* ser;
         HardwareSerial* logSer;
-        Serial_* logSerUSB;
+        #if defined(Serial_)
+            Serial_* logSerUSB;
+        #endif
         bool serIsUSB;
         bool canDebug = true;
         bool debug = false;
