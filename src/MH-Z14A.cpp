fdf05924e0131f3dbac5854fffdda0f5d2832fb8
Jan-NiklasB <44967908+Jan-NiklasB@users.noreply.github.com>
2021-04-12 15:26:57 +0200

    Update MH-Z14A.cpp
    Created conditions for multi platform support and struct for measureing range

--- src/MH-Z14A.cpp
+++ src/MH-Z14A.cpp
@@ -6,33 +6,93 @@
 
 #include "MH-Z14A.h"
 
-MHZ14A::MHZ14A(HardwareSerial &serial)
+typedef struct
 {
-    ser = &serial;
-    canDebug = false;
+    byte b1;
+    byte b2;
+    byte b3;
+    byte b4;
+} D_range;
+ 
+
+MHZ14A::MHZ14A()
+{
 }
 
-MHZ14A::MHZ14A(HardwareSerial &serial, HardwareSerial &logSerial)
+#if (defined _UART_CLASS_) && (defined _USART_CLASS_) && (defined _USBAPI_)
+void MHZ14A::begin(UARTClass &serial, int timeout)
 {
     ser = &serial;
-    serIsUSB = false;
-    logSer = &logSerial;
-}
-
-#if defined(Serial_)
-MHZ14A::MHZ14A(HardwareSerial &serial, Serial_ &logSerial)
-{
-    ser = &serial;
-    serIsUSB = true;
-    logSerUSB = &logSerial;
-}
-#endif
-
-void MHZ14A::begin(int timeout)
-{
-    ser->begin(9600);
     ser->setTimeout(timeout);
 }
+
+void MHZ14A::begin(USARTClass &serial, int timeout)
+{
+    ser = &serial;
+    ser->setTimeout(timeout);
+}
+
+void MHZ14A::begin(UARTClass &serial, Serial_ &logSerial, int timeout)
+{
+    ser = &serial;
+    logSer = &logSerial;
+    canDebug = true;
+    ser->setTimeout(timeout);
+}
+
+void MHZ14A::begin(UARTClass &serial, USARTClass &logSerial, int timeout)
+{
+    ser = &serial;
+    logSer = &logSerial;
+    canDebug = true;
+    ser->setTimeout(timeout);
+}
+
+void MHZ14A::begin(USARTClass &serial, Serial_ &logSerial, int timeout)
+{
+    ser = &serial;
+    logSer = &logSerial;
+    canDebug = true;
+    ser->setTimeout(timeout);
+}
+
+void MHZ14A::begin(USARTClass &serial, UARTClass &logSerial, int timeout)
+{
+    ser = &serial;
+    logSer = &logSerial;
+    canDebug = true;
+    ser->setTimeout(timeout);
+}
+
+void MHZ14A::begin(USARTClass &serial, USARTClass &logSerial, int timeout)
+{
+    ser = &serial;
+    logSer = &logSerial;
+    canDebug = true;
+    ser->setTimeout(timeout);
+}
+
+#else
+#ifdef HardwareSerial_h
+
+void MHZ14A::begin(HardwareSerial &serial, int timeout)
+{
+    ser = &serial;
+    hwser = true;
+    ser->setTimeout(timeout);
+}
+
+void MHZ14A::begin(HardwareSerial &serial, HardwareSerial &logSerial, int timeout)
+{
+    ser = &serial;
+    logSer = &logSerial;
+    canDebug = true;
+    hwser = true;
+    ser->setTimeout(timeout);
+}
+
+#endif
+#endif
 
 void MHZ14A::setDebug(bool state)
 {
@@ -44,20 +104,8 @@ void MHZ14A::log(byte msg[9])
 {
     if(debug) 
     {
-        if(!serIsUSB) for(int i=0; i<9; i++) logSer->print(msg[i], HEX);
-        else
-        {
-            #if defined(Serial_)
-                for(int i=0; i<9; i++) logSerUSB->print(msg[i], HEX);
-            #endif
-        }
-        if(!serIsUSB) logSer->println("");
-        else
-        {
-            #if defined(Serial_)
-                logSerUSB->println("");
-            #endif
-        } 
+        for(int i=0; i<9; i++) logSer->print(msg[i], HEX);
+        logSer->println("");       
     }
 }
 
@@ -81,7 +129,7 @@ int MHZ14A::readConcentrationPPM(byte sensor = 0x01)
     msg[8] = crc(msg);
     log(msg);
     ser->write(msg, 9);
-    int timeout = 60;
+    //int timeout = 60;
     ser->readBytes(msgr, 9);
     log(msgr);
     if (msgr[0] != 0xFF) return -1;
@@ -103,7 +151,7 @@ void MHZ14A::calZeroPoint(byte sensor = 0x01)
     ser->write(msg, 9);
 }
 
-void MHZ14A::calSpanPoint(byte sensor = 0x01, uint8_t span_value_ppm = 2000)
+void MHZ14A::calSpanPoint(byte sensor = 0x01, uint32_t span_value_ppm = 2000)
 {
     byte span_high = span_value_ppm / 256;
     byte span_low = span_value_ppm % 256;
@@ -112,13 +160,14 @@ void MHZ14A::calSpanPoint(byte sensor = 0x01, uint8_t span_value_ppm = 2000)
     ser->write(msg, 9);
 }
 
-void MHZ14A::setDetectionRange(byte sensor = 0x01, byte Range[4])
+void MHZ14A::setDetectionRange(byte sensor = 0x01)
 {
-    byte msg[9] = {0xFF, sensor, 0x99, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    for (int i=4; i<8; i++)
-    {
-        msg[i] = Range[i-4];
-    }
+    setDetectionRange(sensor, MR_5000);
+}
+
+void MHZ14A::setDetectionRange(byte sensor, D_range range)
+{
+    byte msg[9] = {0xFF, sensor, 0x99, 0x00, range.b1, range.b2, range.b3, range.b4, 0x00};
     msg[8] = crc(msg);
     ser->write(msg, 9);
 }
